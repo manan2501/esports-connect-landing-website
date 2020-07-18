35a959818b8df517b5ae774417bb383ddce136b6
Manan Kevadiya <43081866+manan2501@users.noreply.github.com>
2020-07-18 12:12:35 +0530

    Update index.js

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -34,9 +34,6 @@ const NavBar = ({ toggleNavbar, isActive }) => (
             <Link className='navbar-item' to='/about'>
                             About
             </Link>
-            <Link className='navbar-item' to='/pricing'>
-                            Pricing
-            </Link>
             <Link className='navbar-item' to='/blog'>
                             Blog
             </Link>
