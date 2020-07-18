4a83654edbae0a1dc34526923d89e6c17c7bffe4
Manan Kevadiya <43081866+manan2501@users.noreply.github.com>
2020-07-18 12:32:14 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteTitle: 'Esports Connect', // Site title.
   siteTitleAlt: 'Esports ecosystem', // Alternative site title for SEO.
-  siteLogo: '/icons/icon-512x512.png', // Logo used for SEO and manifest.
+  siteLogo: '/icons/esports-connect-logo.png', // Logo used for SEO and manifest.
   siteUrl: 'https://gatsby-starter-business.netlify.com', // Domain of your website without pathPrefix.
   pathPrefix: '', // Prefixes all links. For cases when deployed to example.github.io/gatsby-starter-business/.
   siteDescription: 'Connecting Esports Ecosystem', // Website description used for RSS feeds/meta description tag.
